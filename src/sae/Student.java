8725c993f6aad6cda89a20144837462ea73dbfb1
Quentin Barlet <quentin.barlet.etu@univ-lille.fr>
2022-04-04 11:34:58 +0200

    Ajout classe Student

---
+++ src/sae/Student.java
@@ -0,0 +1,75 @@
+package sae;
+
+/**
+ * Project C-G3
+ *
+ * @author quentin.barlet.etu
+ * The 04 of avril 2022
+ */
+public class Student {
+
+    private String lastName;
+    private String firstName;
+    private int studyLevel;
+    private int absence;
+
+    /**
+     * Default constructor
+     *
+     * @param firstName String
+     * @param lastName  String
+     */
+    public Student(String firstName, String lastName) {
+        this(firstName, lastName, 1);
+    }
+
+    /**
+     * Complete constructor with level parameter
+     *
+     * @param firstName  String
+     * @param lastName   String
+     * @param studyLevel int
+     */
+    public Student(String firstName, String lastName, int studyLevel) {
+
+        if (0 == firstName.length() || 0 == lastName.length()) return;
+        if (studyLevel < 0) return;
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studyLevel = studyLevel;
+        this.absence = 0;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getStudyLevel() {
+        return studyLevel;
+    }
+
+    public void setStudyLevel(int studyLevel) {
+        this.studyLevel = studyLevel;
+    }
+
+    public int getAbsence() {
+        return absence;
+    }
+
+    public void setAbsence(int absence) {
+        this.absence = absence;
+    }
+}
