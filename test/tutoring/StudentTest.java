2cb110eb559228b24f3f87f7cb3dea8c3273aed4
Quentin Barlet <quentin.barlet.etu@univ-lille.fr>
2022-05-05 10:06:31 +0200

    Mise à jour des tests de Student suite aux changement des valeurs par defaut des constructeurs

--- test/tutoring/StudentTest.java
+++ test/tutoring/StudentTest.java
@@ -1,4 +1,5 @@
 package tutoring;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -20,9 +21,9 @@ public class StudentTest {
 
     @BeforeEach
     public void initialization() {
-        s1 = new Student("StudentFirstName1", "StudentLastName1");
-        s2 = new Student("StudentFirstName2", "studentLastName3", 2);
-        s3 = new Student("", "StudentLastName3");
+        s1 = new Student("John1", "Doe1");
+        s2 = new Student("John2", "Doe2", 2);
+        s3 = new Student("", "Doe");
     }
 
     @Test
@@ -40,8 +41,8 @@ public class StudentTest {
         assertEquals(0, s1.getAbsence());
 
         // Names
-        assertEquals("john", s3.getFirstName());
-        s3 = new Student("StudentFirstName3", "");
-        assertEquals("doe", s3.getLastName());
+        assertEquals("", s3.getFirstName());
+        s3 = new Student("John", "");
+        assertEquals("", s3.getLastName());
     }
 }
