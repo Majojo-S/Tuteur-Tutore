0c141d36e48c057d0dd571fba94811a5d6044bea
Quentin Barlet <quentin.barlet.etu@univ-lille.fr>
2022-05-09 16:45:28 +0200

    Ajout de la fonction main (enfin)

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Project C-G3
+ *
+ * @author quentin.barlet.etu
+ * @author marine.sandras.etu
+ * @author simon.bocquet.etu
+ * The 09 mai 2022
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+    }
+}
