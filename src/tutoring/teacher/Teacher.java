a4abf835bab50e0d0e2ab81bc9c736b062860e5b
Marine Sandras <marine.sandras.etu@univ-lille.fr>
2022-05-31 09:02:04 +0200

    Teacher modif

--- src/tutoring/teacher/Teacher.java
+++ src/tutoring/teacher/Teacher.java
@@ -15,63 +15,61 @@ import tutoring.Resource;
  */
 public class Teacher extends Person {
 
-    public static ArrayList<Teacher> teachers = new ArrayList<>();
-    private List<Resource> resources;
+	private List<Resource> resources;
 
-    /**
-     * Create new Teacher with 1 resource
-     *
-     * @param firstName First name of teacher
-     * @param lastName Last name of teacher
-     * @param resource resource of teacher
-     */
-    public Teacher(String firstName, String lastName, Resource resource) {
-        super(firstName, lastName);
-        resources = new ArrayList<>();
-        resources.add(resource);
-    }
+	/**
+	 * Create new Teacher with 1 resource
+	 *
+	 * @param firstName First name of teacher
+	 * @param lastName  Last name of teacher
+	 * @param resource  resource of teacher
+	 */
+	public Teacher(String firstName, String lastName, Resource resource) {
+		super(firstName, lastName);
+		resources = new ArrayList<>();
+		resources.add(resource);
+	}
 
-    /**
-     * Create New Teacher with multiple resources
-     *
-     * @param firstName name of teacher
-     * @param lastName  name of teacher
-     * @param resources of resource of the teacher
-     */
-    public Teacher(String firstName, String lastName, List<Resource> resources) {
-        super(firstName, lastName);
-        this.resources = resources;
-    }
+	/**
+	 * Create New Teacher with multiple resources
+	 *
+	 * @param firstName name of teacher
+	 * @param lastName  name of teacher
+	 * @param resources of resource of the teacher
+	 */
+	public Teacher(String firstName, String lastName, List<Resource> resources) {
+		super(firstName, lastName);
+		this.resources = resources;
+	}
 
+	/**
+	 * Return a list of resources that the teacher teach
+	 *
+	 * @return list of resource of teacher
+	 */
+	public List<Resource> getResources() {
+		return resources;
+	}
 
-    /**
-     * Return a list of resources that the teacher teach
-     *
-     * @return list of resource of teacher
-     */
-    public List<Resource> getResources() {
-        return resources;
-    }
+	/**
+	 * Set the list of resources the teacher can teach
+	 *
+	 * @param resources List of resources to affect to the teacher
+	 */
+	public void setResources(List<Resource> resources) {
+		this.resources = resources;
+	}
 
-    /**
-     * Set the list of resources the teacher can teach
-     *
-     * @param resources List of resources to affect to the teacher
-     */
-    public void setResources(List<Resource> resources) {
-        this.resources = resources;
-    }
-
-    /**
-     * Add a new Resource
-     *
-     * @param resource Resource to add 
-     */
-    public void addResource(String resource) {
-    	for (Resource res : Resource.values()) {
-    		if(res.name().equals(resource.toUpperCase()))
-    			resources.add(res);
+	/**
+	 * Add a new Resource
+	 *
+	 * @param resource Resource to add
+	 */
+	public void addResource(String resource) {
+		for (Resource res : Resource.values()) {
+			if (res.name().equals(resource.toUpperCase()))
+				resources.add(res);
 		}
-    }
+	}
 
 }
